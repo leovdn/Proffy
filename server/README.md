ed20aecf58eaa7a96088446c06f23b9f9d0ebe46
leovdn <leo_vdn@hotmail.com>
2020-08-04 17:16:03 -0300

    database connection config

---
+++ server/README.md
@@ -0,0 +1,14 @@
+# Functionalities
+
+## Connections
+
+- Route to list all executed connections;
+- Route to create a new connection;
+
+## Classes
+
+- Route to create a class;
+- Route to list classes;
+  - Filter by subject, day and time;
+
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('teste server 2')
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({ message: 'Hello Mate'})
+});
+
+app.listen(3030);
