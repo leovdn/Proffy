e0a2174245a18a1e999a7876f8e7e1292de70b10
leovdn <leo_vdn@hotmail.com>
2020-08-03 23:56:22 -0300

    created study and give-classes pages as SPA

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/styles/global.css';
 import Landing from './pages/Landing';
+import Routes from './routes';
 
 function App() {
   return (
-    <Landing />
+    <Routes />
   );
 }
 
 export default App;
+
