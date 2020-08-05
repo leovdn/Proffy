9157fe48b3a0850e8052d9fc802a46a6fecc6d6e
leovdn <leo_vdn@hotmail.com>
2020-08-05 15:45:27 -0300

    create form components; style form page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import './styles.css';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+      <PageHeader 
+        title="Que incrível que você quer dar aulas." 
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre Aula</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />          
+        </fieldset>
+
+        <footer>
+          <p><img src={warningIcon} alt="Aviso!"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar Cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   );
 }
