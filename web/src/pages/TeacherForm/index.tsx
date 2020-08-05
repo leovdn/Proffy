61fcd82b1bcb437feace3a77f6a782ecc2bf3ed8
leovdn <leo_vdn@hotmail.com>
2020-08-05 19:24:11 -0300

    landing page redirection from form submit

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/TextArea';
@@ -8,6 +9,8 @@ import './styles.css';
 import api from '../../services/api';
 
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
@@ -52,6 +55,8 @@ function TeacherForm() {
       schedule: scheduleItems
     }).then(() => {
       alert('Cadastro realizado com sucesso!');
+
+      history.push('/');
     }).catch(() => {
       alert('Erro no cadastro.');
     })
