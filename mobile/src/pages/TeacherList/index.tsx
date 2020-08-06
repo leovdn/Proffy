6d8e7d9714bf13c2a3e098f221c8d4b4e8e0d3ae
leovdn <leo_vdn@hotmail.com>
2020-08-06 20:30:34 -0300

    tab navigation styled

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+function TeacherList() {
+  return (
+    <View style={styles.container} />
+  );
+}
+
+export default TeacherList;
