f0aa425782e61752403590970d4c85e3ee027abc
leovdn <leo_vdn@hotmail.com>
2020-08-07 20:41:49 -0300

    fixed landing page connections via whatsapp contact; finished app

--- mobile/src/pages/TeacherItem/index.tsx
+++ mobile/src/pages/TeacherItem/index.tsx
@@ -7,6 +7,7 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 import styles from './styles';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -27,6 +28,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
   
   function handleLinkToWhatsapp() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
   }
 
