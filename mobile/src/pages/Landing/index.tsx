2dd3fda2cda9d57891143fde8b05c29742f143e4
leovdn <leo_vdn@hotmail.com>
2020-08-07 18:17:07 -0300

    integrated api request into app; built total connections function

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -12,6 +13,15 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
   const navigation = useNavigation();
+  const [ totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
 
   function handleNavigateToGiveClassesPage() {
     navigation.navigate('GiveClasses');
@@ -52,7 +62,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {'  '}
+        Total de {totalConnections} conexões já realizadas {'  '}
         <Image source={heartIcon} /> 
       </Text>
     </View>
