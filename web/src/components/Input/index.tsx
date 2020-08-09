0ee97303abaa4bf6539716a6c0ec7287caac66f1
leovdn <leo_vdn@hotmail.com>
2020-08-09 15:20:41 -0300

    login form styles finished

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -5,13 +5,14 @@ import './styles.css';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
+  inputType?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
+const Input: React.FC<InputProps> = ({ inputType, label, name, ...rest }) => {
   return (
     <div className="input-block">
       <label htmlFor={name}>{label}</label>
-      <input type="text" id={name} {...rest} />
+      <input type={inputType} id={name} {...rest} />
     </div>
   );
 }
--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Input from '../../components/Input';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
+
+
+// const [email, setEmail] = useState("");
+// const [password, setPassword] = useState("");
+// const [remember, setRemember] = useState("");
+
+// useEffect(() => {
+//   console.log(remember);
+// }, [remember]);
+
 
 function Login() {
   return (
@@ -15,7 +28,43 @@ function Login() {
 
       <div className="login-fields">
         <form>
-          <h2>Fazer Login</h2>
+          <h2>Fazer login</h2>
+          <Input
+            inputType="text"
+            name="email"
+            label="E-mail"
+          />
+
+          <Input
+            inputType="password"
+            name="password"
+            label="Senha"
+            className="password-input"
+          />
+
+          <div className="remember-password">
+            <input
+              type="checkbox"
+              id="remember-check"
+              name="remember-check"
+            />
+            <label htmlFor="remember-check">Lembrar-me</label>
+            <a href="#">Esqueci minha senha</a>
+          </div>
+
+          <button>
+            Entrar
+          </button>
+
+          <div className="form-footer">
+            <h2>Nao tem conta?
+              <Link to="/register">Cadastre-se</Link>
+            </h2>
+            <span>É de graça
+            <img src={purpleHeartIcon} alt="Coração Roxo" />
+            </span>            
+          
+          </div>
           
         </form>
       </div>
